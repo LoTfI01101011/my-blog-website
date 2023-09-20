809559e1e91203912fcdf6991eaec590a63e17a9
Lotfi <lotfi.klahe@gmail.com>
2023-09-20 15:51:59 +0100

    refactoring the code 2

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,71 +1,34 @@
 <x-layout>
-  <section class="px-6 py-8">
-    <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-      <form method="Post" action="/register">
-        <h1 class="text-center font-bold text-xl">Register!</h1>
-        @csrf
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10 ">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register!</h1>
+                <form method="Post" action="/register">
 
-        <div class="mb-6">
-          <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
-            Name
-          </label>
+                    @csrf
 
-          <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" value="{{old('name')}}" required>
+                    <x-form.input name="name" autocomplete="name" />
 
-          @error('name')
-          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-          @enderror
-        </div>
+                    <x-form.input name="username" autocomplete="username" />
 
-        <div class="mb-6">
-          <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
-            Username
-          </label>
+                    <x-form.input name="email" type="email" autocomplete="email" />
 
-          <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" value="{{old('username')}}" required>
+                    <x-form.input name="password" type="password" autocomplete="new-password" />
 
-          @error('username')
-          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-          @enderror
-        </div>
+                    <x-submit-button>Register</x-submit-button>
 
-        <div class="mb-6">
-          <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
-            Email
-          </label>
+                    <div class="mt-3">
+                        @if ($errors->any())
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <p class="text-red-500 text-xs">{{ $error }}</p>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </div>
 
-          <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" value="{{old('email')}}" required>
-
-          @error('email')
-          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-          @enderror
-        </div>
-
-        <div class="mb-6">
-          <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
-            Password
-          </label>
-
-          <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" required>
-
-          @error('password')
-          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-          @enderror
-        </div>
-
-        <div class="mb-6">
-          <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-            Submit
-          </button>
-        </div>
-        @if ($errors->any())
-        <ul>
-          @foreach ($errors->all() as $error )
-          <p class="text-red-500 text-xs">{{$error}}</p>
-          @endforeach
-        </ul>
-        @endif
-      </form>
-    </main>
-  </section>
+                </form>
+            </x-panel>
+        </main>
+    </section>
 </x-layout>
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,27 +1,25 @@
 <x-layout>
-  <section class="px-6 py-8">
-    <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Log in!</h1>
+                <form method="Post" action="/login" class="mt-10">
 
-      <form method="Post" action="/login">
-        <h1 class="text-center font-bold text-xl">Log in!</h1>
-        @csrf
-        
-        <x-form.input name="email" type="email" autocomplete="username"/>
-        <x-form.input name="password" type="password" autocomplete="new-password"/>
-        
-        <div class="mb-6">
-          <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-            Submit
-          </button>
-        </div>
-        @if ($errors->any())
-        <ul>
-          @foreach ($errors->all() as $error )
-          <p class="text-red-500 text-xs">{{$error}}</p>
-          @endforeach
-        </ul>
-        @endif
-      </form>
-    </main>
-  </section>
+                    @csrf
+
+                    <x-form.input name="email" type="email" autocomplete="username" />
+                    <x-form.input name="password" type="password" autocomplete="new-password" />
+
+                    <x-submit-button>Log In</x-submit-button>
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <p class="text-red-500 text-xs">{{ $error }}</p>
+                            @endforeach
+                        </ul>
+                    @endif
+                </form>
+            </x-panel>
+        </main>
+    </section>
 </x-layout>
